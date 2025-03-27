c2ca72595e1871f998316d6bbc6482ca3ac7bc0b
ssehaj8 <sehajpreetkaur0812@gmail.com>
2025-03-27 20:14:29 +0530

    [Sehajpreet] Add: Multiple product categories in same

---
+++ src/com/bridgelabz/OnlineMarketplace/Product.java
@@ -0,0 +1,34 @@
+package com.bridgelabz.OnlineMarketplace;
+
+// Generic Product class restricted to specific categories
+public class Product<T extends Enum<T>> {
+    private String name;
+    private double price;
+    private T category;
+
+    public Product(String name, double price, T category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public T getCategory() {
+        return category;
+    }
+
+    public void displayProduct() {
+        System.out.println("Product: " + name + " | Price: Rs." + price + " | Category: " + category);
+    }
+}
---
+++ src/com/bridgelabz/OnlineMarketplace/ProductCategories.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.OnlineMarketplace;
+
+// Categories for Books
+enum BookCategory {
+    FICTION, NON_FICTION, EDUCATIONAL
+}
+
+// Categories for Clothing
+enum ClothingCategory {
+    MEN, WOMEN, KIDS
+}
+
+// Categories for Gadgets
+enum GadgetCategory {
+    MOBILE, LAPTOP, ACCESSORY
+}
