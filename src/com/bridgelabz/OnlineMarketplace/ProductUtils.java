50520891372757078f68c02b5fd218af5e2128e4
ssehaj8 <sehajpreetkaur0812@gmail.com>
2025-03-27 20:21:58 +0530

    [Sehajpreet] Add:generic method to apply discounts dynamically

---
+++ src/com/bridgelabz/OnlineMarketplace/ProductUtils.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.OnlineMarketplace;
+
+public class ProductUtils {
+    public static <T extends Product<?>> void applyDiscount(T product, double percentage) {
+        double discountAmount = product.getPrice() * (percentage / 100);
+        double newPrice = product.getPrice() - discountAmount;
+        product.setPrice(newPrice);
+        System.out.println("Discount applied: " + percentage + "% off on " + product.getName() + ". New Price: $" + newPrice);
+    }
+}
