663833d5f7aa452b8e245a7decdeab6215c9560c
ssehaj8 <sehajpreetkaur0812@gmail.com>
2025-03-27 20:42:06 +0530

    [Sehajpreet] Add:Abstract Class for Course Types

---
+++ src/com/bridgelabz/courseManagementSystem/Course.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.courseManagementSystem;
+
+abstract class CourseType {
+    private String courseName;
+    private String department;
+
+    public CourseType(String courseName, String department) {
+        this.courseName = courseName;
+        this.department = department;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public abstract void displayCourseInfo();
+}
+
